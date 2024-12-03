5b605c33167c8ec4cdf0420e97ec5fafbb6be6f2
Revanthpb <129760083+Revanthpb@users.noreply.github.com>
2024-12-03 15:13:16 +0530

    Add files via upload

---
+++ ChatbotRevanApplication.java
@@ -0,0 +1,11 @@
+package chatbot.revan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotRevanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotRevanApplication.class, args);
+    }
+}
